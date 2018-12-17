f06b0308305e1f440b3bf757d3c17e244a7006a6
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2018-12-18 00:25:19 +0200

    Edit ViewModels

--- app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
@@ -15,10 +15,12 @@
  */
 package com.example.android.sunshine.ui.detail;
 
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import com.example.android.sunshine.AppExecutors;
 import com.example.android.sunshine.R;
 import com.example.android.sunshine.data.database.WeatherEntry;
 import com.example.android.sunshine.databinding.ActivityDetailBinding;
@@ -30,10 +32,10 @@ import java.util.Date;
 /**
  * Displays single day's forecast
  */
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends LifecycleActivity {
 
     public static final String WEATHER_ID_EXTRA = "WEATHER_ID_EXTRA";
-
+    DetailActivityViewModel mViewModel;
     /*
      * This field is used for data binding. Normally, we would have to call findViewById many
      * times to get references to the Views in this Activity. With data binding however, we only
@@ -50,7 +52,28 @@ public class DetailActivity extends AppCompatActivity {
         mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
         long timestamp = getIntent().getLongExtra(WEATHER_ID_EXTRA, -1);
         Date date = new Date(timestamp);
+        mViewModel = ViewModelProviders.of(this).get(DetailActivityViewModel.class);
+        mViewModel.getWeather().observe(this, weatherEntry -> {
+            if (weatherEntry != null) bindWeatherToUI(weatherEntry);
+        });
 
+        AppExecutors.getInstance().diskIO().execute(()-> {
+            try {
+
+                // Pretend this is the network loading data
+                Thread.sleep(4000);
+                Date today = SunshineDateUtils.getNormalizedUtcDateForToday();
+                WeatherEntry pretendWeatherFromDatabase = new WeatherEntry(1, 210, today,88.0,99.0,71,1030, 74, 5);
+                mViewModel.setWeather(pretendWeatherFromDatabase);
+
+                Thread.sleep(2000);
+                pretendWeatherFromDatabase = new WeatherEntry(1, 952, today,50.0,60.0,46,1044, 70, 100);
+                mViewModel.setWeather(pretendWeatherFromDatabase);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private void bindWeatherToUI(WeatherEntry weatherEntry) {
