dd90b338f45b660199bc4b938cd7250e4cedd2b9
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2018-12-19 04:17:01 +0200

    Update WordRoomDatabase and WordViewModel

--- app/src/main/java/com/example/mayada/androidroomwithaview_kotlin/WordRoomDatabase.kt
+++ app/src/main/java/com/example/mayada/androidroomwithaview_kotlin/WordRoomDatabase.kt
@@ -1,9 +1,14 @@
 package com.example.mayada.androidroomwithaview_kotlin
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.IO
+import kotlinx.coroutines.experimental.launch
 
 @Database(entities = arrayOf(Word::class), version = 1)
 public abstract class WordRoomDatabase : RoomDatabase() {
@@ -14,20 +19,49 @@ public abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
-                    "Word_database"
-                ).build()
+                    "word_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
-                return instance
+                // return instance
+                instance
             }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch(Dispatchers.IO) {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+        fun populateDatabase(wordDao: WordDao) {
+            wordDao.deleteAll()
+
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            word = Word("Hi")
+            wordDao.insert(word)
+            word = Word("Android")
+            wordDao.insert(word)
         }
     }
 }
